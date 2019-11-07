fd18cad4ee2e5ed39d702a982c9c082b81df24ff
Guilherme Lacerda <guilherme.lacerda@dbccompany.com.br>
2019-11-07 13:57:10 -0300

    Testes unitários para IterableToCollection.

---
+++ src/test/java/br/com/guilherme/gastos/utils/IterableToCollectionTest.java
@@ -0,0 +1,24 @@
+package br.com.guilherme.gastos.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IterableToCollectionTest {
+
+    @Test
+    void toList() {
+        Iterable iterable = Arrays.asList("Teste1", "Teste2");
+
+        IterableToCollection<String> iterableToCollection = new IterableToCollection<>();
+
+        List<String> strings = iterableToCollection.toList(iterable);
+
+        assertEquals(2, strings.size(), "Tamanho da lista diferente do esperado");
+        assertEquals("Teste1", strings.get(0), "Item 1 da lista diferente do esperado");
+        assertEquals("Teste2", strings.get(1), "Item 2 da lista diferente do esperado");
+    }
+}
