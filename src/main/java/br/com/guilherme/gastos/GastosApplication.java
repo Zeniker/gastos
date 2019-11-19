2dbf59a05b131ed8e55bcbc06a7262fd5d340d31
Guilherme Lacerda <guilherme.lacerda@dbccompany.com.br>
2019-11-19 13:07:26 -0300

    Ajustes nas configurações.

--- src/main/java/br/com/guilherme/gastos/GastosApplication.java
+++ src/main/java/br/com/guilherme/gastos/GastosApplication.java
@@ -3,6 +3,11 @@ package br.com.guilherme.gastos;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,6 +18,15 @@ public class GastosApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GastosApplication.class, args);
+    }
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SPRING_WEB)
+                        .select()
+                        .apis(RequestHandlerSelectors.basePackage("br.com.guilherme.gastos.controller"))
+                        .paths(PathSelectors.any())
+                        .build();
     }
 
 }
