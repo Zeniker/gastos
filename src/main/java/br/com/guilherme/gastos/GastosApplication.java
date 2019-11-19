b152710c6a0508d5ba4782e847a0222af290aa9e
Guilherme Lacerda <guilherme.lacerda@dbccompany.com.br>
2019-11-19 13:05:24 -0300

    Ajustes nas configurações.

--- src/main/java/br/com/guilherme/gastos/GastosApplication.java
+++ src/main/java/br/com/guilherme/gastos/GastosApplication.java
@@ -3,9 +3,11 @@ package br.com.guilherme.gastos;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableSwagger2
 public class GastosApplication {
 
     public static void main(String[] args) {
