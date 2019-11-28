048e01fd5d71a6a9b141d6df428e0520efeb9cc9
Guilherme Lacerda <guilherme.lacerda@dbccompany.com.br>
2019-11-28 10:04:02 -0300

    Adicionada tag @AllArgsConstructor, para eliminar necessidade de criar um construtor para injeção de dependências.

--- src/main/java/br/com/guilherme/gastos/service/categoria/InserirCategoriaService.java
+++ src/main/java/br/com/guilherme/gastos/service/categoria/InserirCategoriaService.java
@@ -4,18 +4,16 @@ import br.com.guilherme.gastos.domain.Categoria;
 import br.com.guilherme.gastos.dto.categoria.CategoriaDTO;
 import br.com.guilherme.gastos.dto.categoria.request.RequestInserirCategoriaDTO;
 import br.com.guilherme.gastos.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class InserirCategoriaService {
 
     private CategoriaRepository categoriaRepository;
-
-    public InserirCategoriaService(CategoriaRepository categoriaRepository) {
-        this.categoriaRepository = categoriaRepository;
-    }
 
     private Categoria inserir(Categoria categoria) {
         categoria.setId(null);
--- src/main/java/br/com/guilherme/gastos/service/categoria/ListarCategoriaService.java
+++ src/main/java/br/com/guilherme/gastos/service/categoria/ListarCategoriaService.java
@@ -3,19 +3,17 @@ package br.com.guilherme.gastos.service.categoria;
 import br.com.guilherme.gastos.domain.Categoria;
 import br.com.guilherme.gastos.dto.categoria.CategoriaDTO;
 import br.com.guilherme.gastos.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class ListarCategoriaService {
 
     private CategoriaRepository categoriaRepository;
-
-    public ListarCategoriaService(CategoriaRepository categoriaRepository) {
-        this.categoriaRepository = categoriaRepository;
-    }
 
     private List<Categoria> listar() {
 
