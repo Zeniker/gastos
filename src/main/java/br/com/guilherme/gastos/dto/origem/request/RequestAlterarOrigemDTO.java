4332c24cd7183068234c877c8e165ff8d78478ce
Guilherme Lacerda <guilherme.lacerda@dbccompany.com.br>
2019-11-28 09:00:09 -0300

    Adicionada coluna tipo de movimentação na origem.

--- src/main/java/br/com/guilherme/gastos/dto/origem/request/RequestAlterarOrigemDTO.java
+++ src/main/java/br/com/guilherme/gastos/dto/origem/request/RequestAlterarOrigemDTO.java
@@ -2,9 +2,12 @@ package br.com.guilherme.gastos.dto.origem.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class RequestAlterarOrigemDTO {
 
+    @NotNull
     private String nome;
 
 }
