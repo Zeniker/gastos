134326b65bb7f8f4df076643df410d82673ffbbc
Guilherme Lacerda <guilherme.lacerda@dbccompany.com.br>
2019-12-02 15:12:54 -0300

    Remoção de comentários desnecessários.

--- src/main/java/br/com/guilherme/gastos/config/WebSecurityConfig.java
+++ src/main/java/br/com/guilherme/gastos/config/WebSecurityConfig.java
@@ -1,13 +1,5 @@
 package br.com.guilherme.gastos.config;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-
-//public class WebSecurityConfig{
-//
-//}
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,21 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-//    @Value("${auth.password}")
-//    private String passwordBasic;
-//
-//    public String getPasswordBasic() {
-//        return passwordBasic;
-//    }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//            .withUser("usuario")
-//            .password(getPasswordBasic())
-//            .roles("USER");
-//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
