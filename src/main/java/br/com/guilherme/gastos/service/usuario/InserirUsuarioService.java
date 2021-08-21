1f4c6d0b903f4b4eee80797beeda805f562d12d0
Guilherme Lacerda <guilherme.lacerda@totvs.com.br>
2021-08-21 11:45:31 -0300

    Alterado para utilizar dozer nos serviços de usuario.

--- src/main/java/br/com/guilherme/gastos/service/usuario/InserirUsuarioService.java
+++ src/main/java/br/com/guilherme/gastos/service/usuario/InserirUsuarioService.java
@@ -4,16 +4,18 @@ import br.com.guilherme.gastos.domain.Usuario;
 import br.com.guilherme.gastos.dto.sessao.request.RequestRegistrarDTO;
 import br.com.guilherme.gastos.exception.EmailJaCadastradoException;
 import br.com.guilherme.gastos.repository.UsuarioRepository;
+import br.com.guilherme.gastos.utils.ModelMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 @Service
 @Slf4j
 public class InserirUsuarioService {
 
-    private UsuarioRepository usuarioRepository;
-    private PasswordEncoder passwordEncoder;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public InserirUsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
@@ -23,23 +25,21 @@ public class InserirUsuarioService {
     /**
      * Registra um novo usuário no sistema
      *
-     * @param requestDTO Dados do novo usuário
+     * @param request Dados do novo usuário
      * @throws EmailJaCadastradoException Exceção lançada quando o e-mail enviado já existir no sistema
      */
-    public void registrar(RequestRegistrarDTO requestDTO) throws EmailJaCadastradoException {
+    public void registrar(RequestRegistrarDTO request) throws EmailJaCadastradoException {
 
-        var optionalUsuario = usuarioRepository.findByEmail(requestDTO.getEmail());
+        var optionalUsuario = this.usuarioRepository.findByEmail(request.getEmail());
 
         if(optionalUsuario.isPresent()){
             throw new EmailJaCadastradoException();
         }
 
-        Usuario usuario = new Usuario();
-        usuario.setEmail(requestDTO.getEmail());
-        usuario.setNome(requestDTO.getNome());
-        usuario.setSenha(passwordEncoder.encode(requestDTO.getSenha()));
+        Usuario usuario = ModelMapper.getMapper().map(request, Usuario.class);
+        usuario.setSenha(this.passwordEncoder.encode(usuario.getSenha()));
 
-        usuarioRepository.save(usuario);
+        this.usuarioRepository.save(usuario);
 
     }
 }
